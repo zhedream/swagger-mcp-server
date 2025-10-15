6b18b740a750282bf5e804a304d13fca14450a75
liuhaozhe <l19517863@163.com>
2025-10-15 11:47:21 +0800

    删除 Swagger js 解析器

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun run
 
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
